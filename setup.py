850ae6eaea632f89d2c7dca13178e39ae7519538
Tarek Ziadé <tarek@ziade.org>
2023-01-27 14:04:18 -0800

    initial boilerplate

---
+++ setup.py
@@ -0,0 +1,36 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+#
+import os
+import sys
+from setuptools import find_packages, setup
+from ghconn import __version__  # NOQA
+
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+
+classifiers = [
+    "Programming Language :: Python",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python :: 3 :: Only",
+]
+
+
+setup(
+    name="elasticsearch-connectors",
+    version=__version__,
+    packages=find_packages(),
+    description=("Elastic Search Connectors."),
+    long_description=long_description,
+    author="Ingestion Team",
+    author_email="tarek@ziade.org",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=classifiers,
+    install_requires=['aiogithubapi'],
+)
